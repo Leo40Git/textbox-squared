2c988ba2181b101f222662ad860ab1098467d0f4
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2022-03-26 18:26:36 +0300

    hm yes, log4j

--- app/shell/src/main/java/adudecalledleo/tbsquared/app/Main.java
+++ app/shell/src/main/java/adudecalledleo/tbsquared/app/Main.java
@@ -1,9 +1,20 @@
 package adudecalledleo.tbsquared.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class Main {
     private Main() { }
 
-    public static void main(String[] args) {
+    public static final Logger LOGGER;
 
+    static {
+        System.setProperty("log4j.skipJansi", "false"); // enable Log4J's Jansi support
+        LOGGER = LoggerFactory.getLogger("textbox-squared");
+    }
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello world!");
+        LOGGER.error("a");
     }
 }
