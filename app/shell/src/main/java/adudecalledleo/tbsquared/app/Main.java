e58578f2876be3e8cd524e2238688df803490098
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2022-04-27 19:21:23 +0300

    plugin-api: add Jackson for later

--- app/shell/src/main/java/adudecalledleo/tbsquared/app/Main.java
+++ app/shell/src/main/java/adudecalledleo/tbsquared/app/Main.java
@@ -1,5 +1,9 @@
 package adudecalledleo.tbsquared.app;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import com.github.zafarkhaja.semver.Version;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +13,13 @@ public final class Main {
 
     public static final Version VERSION = Version.forIntegers(1, 0, 0);
     public static final Version PLUGIN_API_VERSION = Version.forIntegers(1, 0, 0);
+
+    public static final ObjectMapper JACKSON = new ObjectMapper()
+            .registerModules(
+                    new ParameterNamesModule(),
+                    new Jdk8Module(),
+                    new JavaTimeModule()
+            );
 
     public static final Logger LOGGER;
 
