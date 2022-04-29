edcd841fe48647f9a70c22ec01f56e7a2f744fcc
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2022-04-29 16:43:30 +0300

    plugin-api: ConfigEntryType is a record now

--- app/plugin-api/src/main/java/adudecalledleo/tbsquared/app/plugin/api/config/ConfigEntryType.java
+++ app/plugin-api/src/main/java/adudecalledleo/tbsquared/app/plugin/api/config/ConfigEntryType.java
@@ -1,22 +1,8 @@
 package adudecalledleo.tbsquared.app.plugin.api.config;
 
-public interface ConfigEntryType<T> {
-    static ConfigEntryType<Boolean> bool() {
-        return BooleanEntryType.INSTANCE;
-    }
-
-    static ConfigEntryType<Long> integer() {
-        return IntegerEntryType.INSTANCE;
-    }
-
-    static ConfigEntryType<Double> floating() {
-        return FloatingEntryType.INSTANCE;
-    }
-
-    static ConfigEntryType<String> string() {
-        return StringEntryType.INSTANCE;
-    }
-
-    Class<T> valueClass();
-    T defaultValue();
+public record ConfigEntryType<T>(Class<T> valueClass, T defaultValue) {
+    public static final ConfigEntryType<Boolean> BOOLEAN = new ConfigEntryType<>(Boolean.class, false);
+    public static final ConfigEntryType<Long> INTEGER = new ConfigEntryType<>(Long.class, 0L);
+    public static final ConfigEntryType<Double> FLOATING = new ConfigEntryType<>(Double.class, 0.0D);
+    public static final ConfigEntryType<String> STRING = new ConfigEntryType<>(String.class, "");
 }
