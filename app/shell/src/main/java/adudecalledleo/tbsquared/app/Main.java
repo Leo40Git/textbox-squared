d9ad352f3a576581c30c3fa0f72ab2a21a6d7702
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2022-05-06 16:51:02 +0300

    shell: allow parsing JSON5

--- app/shell/src/main/java/adudecalledleo/tbsquared/app/Main.java
+++ app/shell/src/main/java/adudecalledleo/tbsquared/app/Main.java
@@ -2,7 +2,9 @@ package adudecalledleo.tbsquared.app;
 
 import adudecalledleo.tbsquared.app.plugin.api.serialize.module.FaceRecipeModule;
 import adudecalledleo.tbsquared.app.plugin.api.serialize.module.JSemVerModule;
+import com.fasterxml.jackson.core.json.JsonReadFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
@@ -10,7 +12,10 @@ import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 public final class Main {
     private Main() { }
 
-    public static final ObjectMapper JACKSON = new ObjectMapper()
+    public static final ObjectMapper JACKSON = JsonMapper.builder()
+            .enable(JsonReadFeature.ALLOW_JAVA_COMMENTS, JsonReadFeature.ALLOW_SINGLE_QUOTES,
+                    JsonReadFeature.ALLOW_TRAILING_COMMA, JsonReadFeature.ALLOW_UNQUOTED_FIELD_NAMES)
+            .build()
             .registerModules(
                     new ParameterNamesModule(),
                     new Jdk8Module(),
