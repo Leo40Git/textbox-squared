acd20cfa9ab91ec8f10681e4ad4c7fd2600a7373
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2022-05-09 16:50:49 +0300

    plugin-api: hide FaceIconProviderTypeIdResolver in FaceRecipeModule

--- app/plugin-api/src/main/java/adudecalledleo/tbsquared/app/plugin/api/serialize/module/FaceRecipeModule.java
+++ app/plugin-api/src/main/java/adudecalledleo/tbsquared/app/plugin/api/serialize/module/FaceRecipeModule.java
@@ -2,6 +2,7 @@ package adudecalledleo.tbsquared.app.plugin.api.serialize.module;
 
 import java.io.IOException;
 
+import adudecalledleo.tbsquared.app.plugin.api.serialize.recipe.face.FaceIconProviders;
 import adudecalledleo.tbsquared.app.plugin.api.serialize.recipe.face.FaceRecipe;
 import adudecalledleo.tbsquared.face.icon.FaceIconProvider;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
@@ -11,6 +12,7 @@ import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.core.JsonTokenId;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.annotation.JsonTypeIdResolver;
+import com.fasterxml.jackson.databind.jsontype.impl.TypeIdResolverBase;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 public final class FaceRecipeModule extends SimpleModule {
@@ -70,6 +72,44 @@ public final class FaceRecipeModule extends SimpleModule {
                 }
                 gen.writeEndObject();
             }
+        }
+    }
+
+    private static final class FaceIconProviderTypeIdResolver extends TypeIdResolverBase {
+        @Override
+        public String idFromValue(Object value) {
+            if (value instanceof FaceIconProvider iconProvider) {
+                var typeName = FaceIconProviders.getTypeName(iconProvider.getClass());
+                if (typeName == null) {
+                    throw new IllegalArgumentException("Icon provider type \"" + iconProvider.getClass().getName() + "\" is unregistered!");
+                }
+                return typeName;
+            }
+            throw new IllegalArgumentException("Got non-icon provider value?!");
+        }
+
+        @Override
+        public String idFromValueAndType(Object value, Class<?> suggestedType) {
+            @SuppressWarnings("unchecked")
+            var typeName = FaceIconProviders.getTypeName((Class<? extends FaceIconProvider>) suggestedType);
+            if (typeName == null) {
+                throw new IllegalArgumentException("Icon provider type \"" + suggestedType.getName() + "\" is unregistered!");
+            }
+            return typeName;
+        }
+
+        @Override
+        public JavaType typeFromId(DatabindContext context, String id) {
+            var type = FaceIconProviders.getType(id);
+            if (type == null) {
+                throw new IllegalArgumentException("Unknown icon provider type \"" + id + "\"!");
+            }
+            return context.constructType(type);
+        }
+
+        @Override
+        public JsonTypeInfo.Id getMechanism() {
+            return JsonTypeInfo.Id.CUSTOM;
         }
     }
 
